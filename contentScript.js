3ae8822d1fd827e25055806ccbe32d1a1f773661
nult <null@null.0>
2024-10-02 16:54:14 +0530

    tags are hidden by default

--- contentScript.js
+++ contentScript.js
@@ -3,10 +3,11 @@ let all_blocks = document.getElementsByClassName("roundbox sidebox borderTopRoun
 
 let tar_blk = all_blocks[4]; // target block - "Problem tags" block
 
+// tags are hidden by default
 
 // create button
 var btn_new = document.createElement("button")
-btn_new.innerText = "Hide"
+btn_new.innerText = "Show"
 btn_new.id = "tags-hide-btn"
 btn_new.className = "hide-btn"
 // ----
@@ -17,6 +18,9 @@ btn_new.style.position = "absolute";
 
 btn_new.style.top = "3px";
 btn_new.style.right = "20px";
+
+// tags are hidden by default
+tar_blk.children[1].style.visibility = "hidden";
 
 btn_new.onclick = ()=>{
     if (btn_new.innerHTML=="Hide") {
